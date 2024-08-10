eb4ee008047fffb2e1406b6493371cba1e8c7f9d
Guccifer808 <hucciallstars@gmail.com>
2024-08-10 15:06:45 +0300

    Test meta edits

--- src/app/[lang]/contact/layout.js
+++ src/app/[lang]/contact/layout.js
@@ -1,7 +1,8 @@
 export const metadata = {
   title: 'МедСервісГруп',
   description:
-    'Медичні інновації: обладнання для забезпечення найвищого стандарту догляду та діагностики.',
+    // 'Медичні інновації: обладнання для забезпечення найвищого стандарту догляду та діагностики.',
+    'Transform your online presence with Boxstack, a leading web development agency known for creative designs, custom-built solutions, and flawless user experiences. ',
 }
 
 export default function RootLayout({ children }) {
